7962d2a7ef44c5aee1165323439ef2c1368d3934
object-ions <moshikoatia@gmail.com>
2023-08-19 18:27:12 -0600

    specify functions to execute on submit

--- script.js
+++ script.js
@@ -3,14 +3,17 @@ const form = document.getElementById('form');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
-const password2 = document.getElementById('password2');
+const password2 = document.getElementById('password-2');
 
 //Event listeners
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
     checkRequiredField([username, email, password, password2]);
-    checkInputLength(username, 3, 15)
+    checkInputLength(username, 3, 15);
+    checkPasswordMatch(password, password2);
+    checkInputLength(password, 6, 25);
+    checkEmailFormat(email);
 });
 
 
