f268cd7cb083d4924b423fa1384196b875107291
object-ions <moshikoatia@gmail.com>
2023-08-19 18:37:56 -0600

    add main function check req fileds

--- script.js
+++ script.js
@@ -5,15 +5,27 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password-2');
 
+const fieldsArr = [username, email, password, password2]
+
+function checkRequiredField(fieldsArr) {
+    fieldsArr.forEach(field => {
+        if (field.value === '') {
+            showError(field, message);
+        } else {
+            showSuccess(field);
+        }
+    });
+}
+
 //Event listeners
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    checkRequiredField([username, email, password, password2]);
-    checkInputLength(username, 3, 15);
-    checkPasswordMatch(password, password2);
-    checkInputLength(password, 6, 25);
-    checkEmailFormat(email);
+    checkRequiredField(fieldsArr);
+    // checkInputLength(username, 3, 15);
+    // checkPasswordMatch(password, password2);
+    // checkInputLength(password, 6, 25);
+    // checkEmailFormat(email);
 });
 
 
