827478f6c4e1934259d3b7580278c6d47b51b081
object-ions <moshikoatia@gmail.com>
2023-08-21 18:51:15 -0600

    add checkEmail()

--- script.js
+++ script.js
@@ -65,9 +65,20 @@ function checkInputLength(field, min, max) {
 // //Check password match
 function checkPasswordMatch(password, password2) {
     if (password.value !== password2.value || password.value === '') {
+        showError(password, 'Passwords do not match');
         showError(password2, 'Passwords do not match');
     } else {
         showSuccess(password2);
+    }
+}
+
+//Check email format
+function checkEmail(field) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (re.test(field.value.trim())) {
+        showSuccess(field);
+    } else {
+        showError(field, 'Invalid email');
     }
 }
 
@@ -79,7 +90,8 @@ form.addEventListener('submit', (e) => {
     checkInputLength(username, 3, 15);
     checkInputLength(password, 6, 25);
     checkPasswordMatch(password, password2);
-    // checkEmailFormat(email);
+    checkEmailFormat(email);
     showLog();
-    cleanField(fieldsArr)
+    //show popup succses
+    //cleanField(fieldsArr)
 });
